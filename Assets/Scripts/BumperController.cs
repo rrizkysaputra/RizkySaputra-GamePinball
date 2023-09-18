2e6fe7f7debeac9e6fcb8fc42cadd63f00bd9146
rrizkysaputra <rizky58812.rs@gmail.com>
2023-09-18 13:02:38 +0700

    init
    Ball, Paddle, Bumper

---
+++ Assets/Scripts/BumperController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperController : MonoBehaviour
+{
+    public Collider bola;
+    public float multiplier;
+    public Color color;
+
+    public Renderer renderer;
+    public Animator animator;
+
+    private void start()
+    {
+        renderer = GetComponent<Renderer>();
+        animator = GetComponent<Animator>();
+
+        renderer.material.color = color;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider == bola)
+        {
+        Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
+        bolaRig.velocity *= multiplier;
+
+        //animasi
+        animator.SetTrigger("hit");
+        }
+    }
+}
