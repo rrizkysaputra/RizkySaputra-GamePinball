dd318cabfff04df75669854b6b1636dd747125e4
rrizkysaputra <rizky58812.rs@gmail.com>
2023-09-25 02:22:50 +0700

    Switch, Ramp, Launcher

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float maxSpeed;
+    private Rigidbody rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Rigidbody>().velocity.magnitude > maxSpeed)
+        {
+            rig.velocity = GetComponent<Rigidbody>().velocity.normalized * maxSpeed;
+        }
+    }
+}
